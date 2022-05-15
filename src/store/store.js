e707f5c3cb851adeba594b17e90366be280c6247
y2k01 <scotlandfjord@protonmail.com>
2022-05-15 22:13:34 +0900

    Add pinia store

---
+++ src/store/store.js
@@ -0,0 +1,17 @@
+import { defineStore } from 'pinia'
+
+// useStore could be anything like useUser, useCart
+// the first argument is a unique id of the store across your application
+export const useStore = defineStore('counter', {
+  state: () => {
+    return {
+      name: 'hi',
+      count: 0,
+    }
+  },
+  actions: {
+    increment() {
+      this.count++
+    }
+  }
+})

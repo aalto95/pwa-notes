e2d52423ed9db415b2cb2a194144fec601c066e2
y2k01 <scotlandfjord@protonmail.com>
2022-06-19 16:24:38 +0900

    Add PWA

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'virtual:windi.css'
+import './registerServiceWorker'
 
 const app = createApp(App)
 app.use(createPinia())

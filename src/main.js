5444525b6248396c59608009df43471c0612f77b
y2k01 <scotlandfjord@protonmail.com>
2022-06-19 17:41:35 +0900

    Get rid of service worker

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'virtual:windi.css'
-import './registerServiceWorker'
 
 const app = createApp(App)
 app.use(createPinia())

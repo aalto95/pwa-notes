13aa5a9a010b0962da04cd9fd4d0bb159a615c8c
aaltoo <scotlandfjord@protonmail.com>
2022-11-08 18:00:07 +0900

    Rework header and Add note creation button there

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,26 @@
 <template>
-  <header class="w-full h-15 text-white flex fixed z-10">
-    <h1
-      class="text-xl bg-black w-full h-full flex justify-center items-center text-center"
+  <header
+    class="w-full h-15 text-white flex justify-between items-center fixed z-10 bg-black px-4"
+  >
+    <router-link to="/">
+      <h1 class="text-xl font-bold">Notes</h1>
+    </router-link>
+    <button
+      class="bg-light-500 rounded-full w-10 h-10 flex justify-center items-center"
+      @click="openNewNoteView()"
     >
-      Notes
-    </h1>
+      <PlusIcon class="h-6 w-6 text-black" />
+    </button>
   </header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import router from "../router";
+import { PlusIcon } from "@heroicons/vue/24/solid";
+
+function openNewNoteView() {
+  router.push("/note");
+}
+</script>
 
 <style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,10 @@
   <div class="mt-15">
     <Notes />
   </div>
-  <AddNoteButton />
 </template>
 
 <script setup lang="ts">
 import Notes from "../components/Notes.vue";
-import AddNoteButton from "../components/AddNoteButton.vue";
 </script>
 
 <style scoped></style>

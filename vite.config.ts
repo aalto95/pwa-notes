c110cb5d35ce20e3911e055f23e21d3e9f685d7c
aalto95 <stanportableuse@gmail.com>
2025-02-03 15:38:28 +0900

    Add bundle analyzer and update vue

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from "@tailwindcss/vite";
+import { analyzer } from "vite-bundle-analyzer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -50,5 +51,6 @@ export default defineConfig({
         ],
       },
     }),
+    analyzer(),
   ],
 });
